3d6894d69be590a1e9681f57baff8f7fbb096dd8
yassshhhh15 <yashpednekar264@gmail.com>
2024-09-30 08:23:24 +0000

    spartan: Add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/yassshhhh15/proprietary_vendor_realme_spartan.git vendor/realme/spartan --depth=1
+git clone https://github.com/yassshhhh15/android_hardware_oplus.git hardware/oplus
+git clone https://github.com/yassshhhh15/android_kernel_realme_sm8250.git kernel/realme/sm8250 --depth=1
