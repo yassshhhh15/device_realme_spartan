83f0cc445a56c706feb2f3ac18fd9a65dc2d7f46
yassshhhh15 <yashpednekar264@gmail.com>
2024-10-05 04:10:03 +0000

    spartan: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-git clone https://github.com/yassshhhh15/proprietary_vendor_realme_spartan.git vendor/realme/spartan --depth=1
-git clone https://github.com/yassshhhh15/android_hardware_oplus.git hardware/oplus
+git clone https://github.com/yassshhhh15/proprietary_vendor_realme_spartan.git -b rising-14-gcam vendor/realme/spartan --depth=1
+git clone https://github.com/yassshhhh15/android_hardware_oplus.git -b rising-14 hardware/oplus
 git clone https://github.com/yassshhhh15/android_kernel_realme_sm8250.git kernel/realme/sm8250 --depth=1
