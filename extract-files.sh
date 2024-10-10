e59590aec104495de0d58c4b2074c3d2fabefc7c
yograjfire18 <yograjfire18@gmail.com>
2024-10-10 18:25:12 +0530

    fixup! spartan: Sync extract scripts with 22 templates

--- extract-files.sh
+++ extract-files.sh
@@ -109,7 +109,6 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
             :;
-        *)
             return 1
             ;;
     esac
